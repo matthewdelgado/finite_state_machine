b64f7b3c0117857744615aa40f67c721af513ee0
Matthew Delgado <mdelgado2k@gmail.com>
2022-09-20 17:33:52 -0400

    added comments

--- main.py
+++ main.py
@@ -7,8 +7,8 @@
 from StateMachine import StateMachine
 
 # adjective lists
-positive_adjectives = ["great","super", "fun", "entertaining", "easy"]
-negative_adjectives = ["boring", "difficult", "ugly", "bad"]
+positive_adj = ["great","super", "fun", "entertaining", "easy"]
+negative_adj = ["boring", "difficult", "ugly", "bad"]
 
 # Check the argument for the correct transition function value.
 # if transition to "Python State" return newState
@@ -27,7 +27,7 @@ def start_transitions(txt):
 # else set newState equals “error_state”
 # function should return newState and the input "txt"
 def python_state_transitions(txt):
-    split_txt = txt.split(None,1)
+    split_txt = txt.split(None, 1)
     word, txt = split_txt if len(split_txt) > 1 else (txt,"")
     if word == "is":
         newState = "is_state"
@@ -43,13 +43,13 @@ def python_state_transitions(txt):
 # else set newState equals “error_state”
 # function should return newState and the input "txt"
 def is_state_transitions(txt):
-    split_txt = txt.split(None,1)
+    split_txt = txt.split(None, 1)
     word, txt = split_txt if len(split_txt) > 1 else (txt,"")
     if word == "not":
         newState = "not_state"
-    elif word in positive_adjectives:
+    elif word in positive_adj:
         newState = "pos_state"
-    elif word in negative_adjectives:
+    elif word in negative_adj:
         newState = "neg_state"
     else:
         newState = "error_state"
@@ -61,25 +61,29 @@ def is_state_transitions(txt):
 # else set newState equals “error_state”
 # function should return newState and the input "txt"
 def not_state_transitions(txt):
-    split_txt = txt.split(None,1)
+    split_txt = txt.split(None, 1)
     word, txt = split_txt if len(split_txt) > 1 else (txt,"")
-    if word in positive_adjectives:
+    if word in positive_adj:
         newState = "neg_state"
-    elif word in negative_adjectives:
+    elif word in negative_adj:
         newState = "pos_state"
     else:
         newState = "error_state"
     return (newState, txt)
 
+# declare new object m
 m = StateMachine()
+# add states
 m.add_state("Start", start_transitions)
 m.add_state("Python_state", python_state_transitions)
 m.add_state("is_state", is_state_transitions)
 m.add_state("not_state", not_state_transitions)
-m.add_state("neg_state", None, end_state=1)
-m.add_state("pos_state", None, end_state=1)
-m.add_state("error_state", None, end_state=1)
+m.add_state("neg_state", None, end_state = 1)
+m.add_state("pos_state", None, end_state = 1)
+m.add_state("error_state", None, end_state = 1)
+# set starting state
 m.set_start("Start")
+# run tests
 m.run("Python is great")
 m.run("Python is difficult")
 m.run("Perl is ugly")
